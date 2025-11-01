ffeabf8dc3a881eb8c68cb0a6b8ef6f5bc1a5077
João Victor Lopez Pereira <joaovictorlopezpereira@gmail.com>
2025-11-01 14:10:45 -0300

    Adiciona generate_tests.py

---
+++ generate_tests.py
@@ -0,0 +1,42 @@
+import struct
+
+# Cria um arquivo binário no formato específico de entrada para o programa de Isomorfismo
+def create_binary(file_name,
+                  print_isomorphism, show_time, n_cons_threads, buffer_size,
+                  nv1, na1, list1,
+                  nv2, na2, list2):
+    with open(file_name, "wb") as f:
+        # Parâmetros do programa
+        f.write(struct.pack("4i", print_isomorphism, show_time, n_cons_threads, buffer_size))
+        f.write(b"\n")
+
+        # Parâmetros sobre o primeiro grafo
+        f.write(struct.pack("2i", nv1, na1))
+        f.write(b"\n")
+        for x, y in list1[:na1]:
+            f.write(struct.pack("2i", x, y))
+            f.write(b"\n")
+
+        # Parâmetros sobre o segundo grafo
+        f.write(struct.pack("2i", nv2, na2))
+        for x, y in list2[:na2]:
+            f.write(b"\n")
+            f.write(struct.pack("2i", x, y))
+
+    print(f"created file {file_name}")
+
+# Teste 1 (triângulo) (isomorfo)
+create_binary(
+    "file1.bin",
+    1, 1, 4, 8,
+    3, 3, [[0, 1], [1, 2], [2, 0]],
+    3, 3, [[0, 1], [2, 0], [2, 1]]
+)
+
+# Teste 2 (Grafo gigantesco esparso com 1 ligação) (isomorfo)
+create_binary(
+    "file2.bin",
+    1, 1, 4, 8,
+    1000, 1, [[67, 92]],
+    1000, 1, [[0, 1]]
+)
