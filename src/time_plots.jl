6e29e461c616326f9d2133bb0d6b09e0264aeae1
João Victor Lopez Pereira <joaovictorlopezpereira@gmail.com>
2025-11-03 23:07:14 -0300

    Adiciona testes e mais medições de métricas

--- src/time_plots.jl
+++ src/time_plots.jl
@@ -16,8 +16,15 @@ function print_tests(test_i_results)
     # Garante que todos os testes tem o mesmo tamanho
     @assert length(test_i_results.sequential) == length(test_i_results.t_index1) == length(test_i_results.t_index2)
 
+    # Calcula os tempos médios
+    seq_time = mean(test_i_results.sequential)
+    index_1_time = mean(test_i_results.t_index1)
+    index_2_time = mean(test_i_results.t_index2)
+    acc_index_1 = seq_time / index_1_time
+    acc_index_2 = seq_time / index_2_time
+
     # Imprime os tempos médios e outras métricas
-    @printf("\n>8----------------------------8<\n")
+    @printf("\n>8--------------------------------8<\n")
     @printf("  Tempo médio de %d testes:\n", length(test_i_results.sequential))
     if (test_i_results.isomorphic_or_no == 1)
         @printf("  Grafos isomorfos\n")
@@ -26,10 +33,14 @@ function print_tests(test_i_results)
     end
     @printf("  Número de Vértices : %d\n", test_i_results.number_of_vertices)
     @printf("  Número de Arestas  : %d\n", test_i_results.number_of_edges)
-    @printf("  Tempo do Sequencial: %f\n", mean(test_i_results.sequential))
-    @printf("  Tempo do t_index1  : %f\n", mean(test_i_results.t_index1))
-    @printf("  Tempo do t_index2  : %f\n", mean(test_i_results.t_index2))
-    @printf(">8----------------------------8<\n")
+    @printf("  Tempo do Sequencial: %f\n", seq_time)
+    @printf("  Tempo do t_index1  : %f\n", index_1_time)
+    @printf("  Tempo do t_index2  : %f\n", index_2_time)
+    @printf("  Aceleração do t_index1  : %f\n", acc_index_1)
+    @printf("  Aceleração do t_index2  : %f\n", acc_index_2)
+    @printf("  Eficiência do t_index1  : %f\n", acc_index_1 / test_i_results.number_of_vertices)
+    @printf("  Eficiência do t_index2  : %f\n", acc_index_2 / test_i_results.number_of_vertices)
+    @printf(">8--------------------------------8<\n")
 end
 
 # isomorfo_cheio.txt
@@ -64,9 +75,46 @@ test_4_results = Test(11,
                       [5.563325e+00, 5.570441e+00, 5.567013e+00, 5.591806e+00, 5.565319e+00],
                       [3.136392e-01, 3.073802e-01, 3.067329e-01, 3.078449e-01, 3.072031e-01])
 
+# Prints the results of tests with 11 vertices
 print_tests(test_1_results)
 print_tests(test_2_results)
 print_tests(test_3_results)
 print_tests(test_4_results)
 
-# test_02 is missing
+# isomorfo_cheio.txt
+test_5_results = Test(9,
+                      64,
+                      1,
+                      [1.492691e-02, 1.335311e-02, 1.416993e-02, 1.391602e-02, 1.360893e-02],
+                      [3.869796e-02, 3.953815e-02, 4.103804e-02, 3.997493e-02, 3.832388e-02],
+                      [4.677057e-03, 3.771067e-03, 4.271030e-03, 3.926992e-03, 3.906965e-03])
+
+# isomorfo_esparso.txt
+test_6_results = Test(9,
+                      5,
+                      1,
+                      [1.365209e-02, 1.358604e-02, 1.333213e-02, 1.425719e-02, 1.398301e-02],
+                      [4.014707e-02, 4.016900e-02, 4.061508e-02, 3.828406e-02, 3.896499e-02],
+                      [3.797054e-03, 4.037142e-03, 4.169941e-03, 3.988981e-03, 4.384041e-03])
+
+# nao_isomorfo_cheio.txt
+test_7_results = Test(9,
+                      64,
+                      0,
+                      [1.010799e-02, 1.055789e-02, 1.005101e-02, 1.072717e-02, 9.484053e-03],
+                      [3.963900e-02, 4.029298e-02, 3.982401e-02, 3.993797e-02, 4.106498e-02],
+                      [5.614042e-03, 6.345987e-03, 6.546021e-03, 6.829023e-03, 6.268024e-03])
+
+# nao_isomorfo_esparso.txt
+test_8_results = Test(9,
+                      1,
+                      0,
+                      [1.245594e-02, 1.307607e-02, 1.300311e-02, 1.241994e-02, 1.352692e-02],
+                      [4.680896e-02, 4.754400e-02, 4.728603e-02, 4.705882e-02, 4.529905e-02],
+                      [4.279137e-03, 3.726959e-03, 4.319191e-03, 4.297972e-03, 4.025936e-03])
+
+# Prints the results of tests with 9 vertices
+print_tests(test_5_results)
+print_tests(test_6_results)
+print_tests(test_7_results)
+print_tests(test_8_results)
